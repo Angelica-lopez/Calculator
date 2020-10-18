141bf6191561a7b071eb85ceb690ce515b98760b
Angelica <angielopez4004@gmail.com>
2020-10-18 14:41:06 -0500

    clean elements of js

--- index.js
+++ index.js
@@ -1,42 +1,5 @@
-
-/*var $screenOfOutcome = document.querySelector("js-screen");
-var $numberZero = document.querySelector(".js-zero");
-var $numberOne = document.querySelector(".js-one");
-var $numberTwo = document.querySelector(".js-two");
-var $numberThree = document.querySelector(".js-three");
-var $numberFour = document.querySelector(".js-four");
-var $numberFive = document.querySelector(".js-five");
-var $numberSix = document.querySelector(".js-six");
-var $numberSeven= document.querySelector(".js-seven");
-var $numberEight = document.querySelector(".js-eight");
-var $numberNine = document.querySelector(".js-nine");
-var $numberMore = document.querySelector(".js-more");
-var $numberLess = document.querySelector(".js-less");
-var $numberFor = document.querySelector(".js-for");
-var $numberSlash = document.querySelector(".js-slash");
-var $numberEqual = document.querySelector(".js-equal");
-var $numberCe = document.querySelector(".js-ce");
-
-
-var contenidoOne = $numberOne.innerHTML;
-var contenidoTwo = $numberTwo.innerHTML;
-var contenidoThree = $numberThree.innerHTML;
-var contenidoFour = $numberFour.innerHTML;
-var contenidoFive = $numberFive.innerHTML;
-var contenidoSix = $numberSix.innerHTML;
-var contenidoSeven = $numberSeven.innerHTML;
-var contenidoEight = $numberEight.innerHTML;
-var contenidoNine = $numberNine.innerHTML;
-var contenidoMore = $numberMore.innerHTML;
-var contenidoLess = $numberLess.innerHTML;
-var contenidoFor = $numberFor.innerHTML;
-var contenidoSlash = $numberSlash.innerHTML;
-var contenidoEqual = $numberEqual.innerHTML;
-var contenidoCe = $numberCe.innerHTML;*/
-
-
-
-
+var uno = document.querySelector("js-seven");
+console.log(uno);
 
 
 
